8839b6fb695401ba8714398f05a57dc41b3930ea
rhinehardtc <rhinehardtc@gmail.com>
2020-12-23 21:46:36 -0600

    added snow and styling

--- xmas/src/App.js
+++ xmas/src/App.js
@@ -3,6 +3,7 @@ import React, {useState, useEffect} from 'react';
 function App() {
   const [user, setUser] = useState('');
   const [login, setLogin] = useState(false);
+  const [placeholder, setPlaceholder] = useState('enter name')
 
   const handleInputChange = (e) => {
       e.target.value ? setUser(e.target.value[0].toUpperCase() + e.target.value.slice(1)) : setUser(e.target.value);
@@ -15,6 +16,18 @@ function App() {
       case "Melissa": 
         setUser("Mom")
         break;
+      case "Jacob": 
+        setUser("Girbin Zambeeze la Ploop the Rhinocerus Pinch")
+      break;
+      case "Jake": 
+        setUser("Girbin Zambeeze la Ploop the Rhinocerus Pinch")
+      break;
+      case "Joey": 
+        setUser("Josh")
+      break;
+      case "Joseph": 
+        setUser("Joshua")
+      break;
       case "Ciara":
         setUser("now Brabe")
         break;
@@ -27,26 +40,26 @@ function App() {
 
   const handleRejection = (e) => {
     e.preventDefault()
-    alert("Surely you have a name...")
+    setPlaceholder("I said 'enter name'!")
   }
 
   const checkLogin = () => {
     if(login === false){
       return(
         <form onSubmit={(e) => user !== '' ? handleLogin(e) : handleRejection(e)}>
-          <input className="name_input" type="text" placeholder="enter name" value={user} onChange={(e) => handleInputChange(e)}></input>
+          <input className="name_input" type="text" placeholder={placeholder} value={user} onChange={(e) => handleInputChange(e)}></input>
           <button className="submit_name_btn" type="submit">Submit</button>
         </form>
       )
     } else {
       return(
-        <h1>Merry Christmas {user}!</h1>
+        <h1 className="merry_xmas">Merry Christmas {user}!</h1>
       )
     }
   }
 
   return (
-    <div className="App">
+    <div className="App snow">
       {checkLogin()}
     </div>
   );
